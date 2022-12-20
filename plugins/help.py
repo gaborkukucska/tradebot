97930e704e2e12073591b96a572004e5d0454172
Gabor Kukucska <gabor@buyahmedia.com>
2022-12-20 22:36:58 +0800

    Create help.py

---
+++ plugins/help.py
@@ -0,0 +1,5 @@
+def main():
+    print('Bob is a software for trading ETH on Binance using the AUD trading pair.')
+    print('To trade, enter "x" at the prompt and then enter "buy" or "sell" to place a trade.')
+    print('To load a plugin, enter "p" at the prompt and enter the name of the plugin to load.')
+    print('To quit the software, enter "q" at the prompt.')
